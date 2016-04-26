b07160ab3c4d84fb8b817bf3e099e6940ea63d9e
William Puk <williampuk@gmail.com>
2016-04-26 16:34:00 +0800

    Added axis to simple bar chart

--- js/webapp.js
+++ js/webapp.js
@@ -3,65 +3,105 @@ var app = function(d3, dimple, $, undefined) {
 
     function updateSurvivalCount(data) {
         var survived = d3.sum(data, function(d) {
-            return d.Survived
+            return d.Survived;
         });
         return [{
-            category: "survived",
+            category: "Survived",
             count: survived
         }, {
-            category: "perished",
+            category: "Perished",
             count: data.length - survived
         }];
     }
 
-    function draw(data) {
-        var numberOfRows = data.length;
-        var survivalGraph = {
-            marginTop: 20,
-            marginLeft: 20,
-            width: 1000,
-            height: 100,
-            colors: {
-                survived: "blue",
-                perished: "red"
-            }
-        };
-        var survivalData = updateSurvivalCount(data);
-        survivalGraph.svg = d3.select("div#survival")
+    function drawSurvivalChart(data) {
+        var marginTop = 20,
+            marginLeft = 50,
+            axisPadding = 0,
+            width = 1000,
+            height = 100,
+            colors = {
+                Survived: "blue",
+                Perished: "red"
+            },
+            numberOfRows = data.length,
+            survivalData = updateSurvivalCount(data);
+        var svg = d3.select("div#survival")
             .append("svg")
-            .attr("width", survivalGraph.width + survivalGraph.marginLeft * 2)
-            .attr("height", survivalGraph.height + survivalGraph.marginTop * 2);
-        survivalGraph.svgGrp = survivalGraph.svg.append("g")
+            .attr("width", width + marginLeft * 2)
+            .attr("height", height + marginTop * 2);
+        var svgGrp = svg.append("g")
             .attr({
-                transform: "translate(" + survivalGraph.marginLeft + "," +
-                    survivalGraph.marginTop + ")",
+                transform: "translate(" + marginLeft + "," +
+                    marginTop + ")",
                 class: 'main-group'
             });
-        survivalGraph.xScale = d3.scale
+        var xScale = d3.scale
             .linear()
-            .domain([0, d3.max(survivalData, function(d) { return d.count; })])
-            .range([0, survivalGraph.width]);
-        debugger;
-        survivalGraph.yScale = d3.scale.ordinal()
-            .domain(d3.range(2))
-            .rangeRoundBands([0, survivalGraph.height], 0.05);
-        survivalGraph.barGroups = survivalGraph.svgGrp.selectAll("g")
+            .domain([0, d3.max(survivalData, function(d) {
+                return d.count;
+            })])
+            .range([0, width]);
+
+        var yScale = d3.scale.ordinal()
+            .domain(survivalData.map(function(d) {
+              return d.category;
+            }))
+            .rangeRoundBands([0, height], 0.2);
+        var barGroups = svgGrp.selectAll("g")
             .data(survivalData)
             .enter()
             .append("g")
             .style("fill", function(d, i) {
-                return survivalGraph.colors[d.category];
+                return colors[d.category];
             });
-        survivalGraph.barGroups
+        barGroups
             .append("rect")
             .attr("x", 0)
             .attr("y", function(d, i) {
-                return survivalGraph.yScale(i);
+                return yScale(d.category);
             })
             .attr("height", function(d) {
-                return survivalGraph.yScale.rangeBand();
+                return yScale.rangeBand();
             })
-            .attr("width", function(d) { return survivalGraph.xScale(d.count); });
+            .attr("width", function(d) {
+                return xScale(d.count);
+            });
+
+        var xAxis = d3.svg.axis()
+            .orient('bottom')
+            .scale(xScale)
+            .ticks(20);
+
+        svg.append('g')
+            .attr({
+                transform: 'translate(' + marginLeft + ',' + (marginTop + height + axisPadding) + ')'
+            })
+            .attr("class", "x axis")
+            .call(xAxis);
+
+        var yAxis = d3.svg.axis()
+            .orient('left')
+            .scale(yScale);
+
+        svg.append('g')
+            .attr({
+                transform: 'translate(' + (marginLeft - axisPadding) + ',' + marginTop  + ')'
+            })
+            .attr("class", "y axis")
+            .call(yAxis);
+        return {
+            marginTop: marginTop,
+            marginLeft: marginLeft,
+            width: width,
+            height: height,
+            colors: colors
+        };
+    }
+
+    function draw(data) {
+        var survivalData = updateSurvivalCount(data);
+        drawSurvivalChart(data);
     }
 
     function init() {
