60e7ae3df1383930feed418f55c8ded8fd326f94
William Puk <williampuk@gmail.com>
2016-05-05 01:36:39 +0800

    Updated link so that it requires no modification to be hosted on github

--- js/webapp.js
+++ js/webapp.js
@@ -865,7 +865,7 @@ var app = function(d3, $) {
   }
 
   function init() {
-    d3.csv('/data/titanic.csv', function(row) {
+    d3.csv('data/titanic.csv', function(row) {
       row.PassengerId = +row.PassengerId;
       row.Survived = +row.Survived;
       row.SurvivedBool = row.Survived === 1;
